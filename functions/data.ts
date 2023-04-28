206d1eee4906603b01854c9d7df4e0075ffda244
dxtmisha <9943154+dxtmisha@users.noreply.github.com>
2023-04-28 14:46:34 +0700

    0.0.1-dev-1

--- functions/data.ts
+++ functions/data.ts
@@ -306,3 +306,14 @@ export function maxListLength (data: AssociativeOrArrayType<string>): number {
     ?.sort((a: number, b: number) => a > b ? -1 : 1)
     ?.[0]
 }
+
+/**
+ * Generate a random integer
+ *
+ * Генерирует случайное число
+ * @param min the lowest value to return / наименьшее значение
+ * @param max the highest value to return / наибольшее значение
+ */
+export function random (min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
