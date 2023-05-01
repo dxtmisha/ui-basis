c1d73942ace452f9e10c17c966a2a29705b4dc23
dxtmisha <9943154+dxtmisha@users.noreply.github.com>
2023-05-01 15:49:07 +0700

    + Env
    Class for working with Env variables from Vue
    Класс для работы с Env переменными от Vue

---
+++ classes/Env.ts
@@ -0,0 +1,48 @@
+/**
+ * Class for working with Env variables from Vue
+ *
+ * Класс для работы с Env переменными от Vue
+ */
+export class Env {
+  /**
+   * Get values by key without the prefix VUE_APP_
+   *
+   * Получить значения по ключу без приставки VUE_APP_
+   * @param key key / ключ
+   * @param value returns value when the value is absent / возвращает value при
+   * отсутствии значения
+   */
+  static get (key: string, value?: string): string {
+    return process.env?.[`VUE_APP_${key}`] || value || ''
+  }
+
+  /**
+   * Get values as a number
+   *
+   * Получить значения в виде числа
+   * @param key key / ключ
+   * @param value returns value when the value is absent / возвращает value при
+   * отсутствии значения
+   */
+  static getInt (key: string, value = 0 as number): number {
+    return parseFloat(this.get(key, value.toString())) || 0
+  }
+
+  /**
+   * Returns CACHE
+   *
+   * Возвращает CACHE
+   */
+  static cache (): number {
+    return this.getInt('CACHE', 7 * 24 * 60 * 60)
+  }
+
+  /**
+   * Returns PREFIX
+   *
+   * Возвращает PREFIX
+   */
+  static prefix (): string {
+    return this.get('PREFIX', '_d_')
+  }
+}
---
+++ classes/StorageItemAbstract.ts
@@ -0,0 +1,10 @@
+import { StorageAbstract } from './StorageAbstract'
+
+/**
+ * Abstract class for working with storage
+ *
+ * Абстрактный класс для работы с хранилищем
+ */
+export abstract class StorageItemAbstract<T = any> extends StorageAbstract<T> {
+
+}
