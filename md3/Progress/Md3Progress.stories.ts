3403048e21e2500a595b2b9bf8d35f7ac7b166e3
dxtmisha <9943154+dxtmisha@users.noreply.github.com>
2023-08-06 01:50:38 +0700

    Md3Progress: fix

--- md3/Progress/Md3Progress.stories.ts
+++ md3/Progress/Md3Progress.stories.ts
@@ -18,7 +18,8 @@ const meta = {
         step: 5
       },
       table: {
-        category: 'Value'
+        category: 'Value',
+        type: { summary: 'number' }
       }
     },
     max: {
@@ -28,55 +29,95 @@ const meta = {
         max: 500,
         step: 50
       },
-      description: 'Maximum permissible value<br>Максимально допустимое значение'
+      description: 'Maximum permissible value<br><br>' +
+        'Максимально допустимое значение',
+      table: {
+        category: 'Value',
+        defaultValue: { summary: '100' },
+        type: { summary: 'number' }
+      }
     },
-    visible: { control: 'boolean' },
+    visible: {
+      control: 'boolean',
+      table: {
+        category: 'Status',
+        type: { summary: 'boolean' }
+      }
+    },
     delay: {
       control: {
         type: 'number',
         min: 0,
         step: 100
       },
-      description: 'Bootloader display delay<br>Задержка отображения загрузчика'
+      description: 'Bootloader display delay<br><br>' +
+        'Задержка отображения загрузчика',
+      table: {
+        category: 'Options',
+        defaultValue: { summary: '400' },
+        type: { summary: 'number' }
+      }
     },
-    ...{
-      // [!] System label, cannot be deleted
-      // [!] Системная метка, нельзя удалять
-      // :arg-types
-      linear: { control: 'boolean' },
-      circular: { control: 'boolean' },
-      indeterminate: {
-        control: 'select',
-        options: ['type1', 'type2']
-      },
-      position: {
-        control: 'select',
-        options: ['top', 'bottom']
-      },
-      dense: { control: 'boolean' },
-      inverse: { control: 'boolean' }
-      // :arg-types
+    linear: {
+      control: 'boolean',
+      table: {
+        category: 'Style',
+        defaultValue: { summary: 'true' },
+        type: { summary: 'boolean' }
+      }
+    },
+    circular: {
+      control: 'boolean',
+      table: {
+        category: 'Style',
+        type: { summary: 'boolean' }
+      }
+    },
+    indeterminate: {
+      control: 'select',
+      options: ['type1', 'type2'],
+      table: {
+        category: 'Style',
+        defaultValue: { summary: 'type1' },
+        type: { summary: 'type1 | type2' }
+      }
+    },
+    position: {
+      control: 'select',
+      options: ['top', 'bottom'],
+      table: {
+        category: 'Style',
+        defaultValue: { summary: 'top' },
+        type: { summary: 'top | bottom' }
+      }
+    },
+    dense: {
+      control: 'boolean',
+      table: {
+        category: 'Style',
+        type: { summary: 'boolean' }
+      }
+    },
+    inverse: {
+      control: 'boolean',
+      table: {
+        category: 'Style',
+        type: { summary: 'boolean' }
+      }
     }
   },
   args: {
-    max: 100,
-    visible: true,
-    delay: 400,
-    ...{
-      // [!] System label, cannot be deleted
-      // [!] Системная метка, нельзя удалять
-      // :default
-      linear: true,
-      indeterminate: 'type1',
-      position: 'top'
-      // :default
-    }
+    visible: true
   }
 } satisfies Meta<typeof Md3Progress>
 
 type Story = StoryObj<typeof meta>;
 
 export default meta
+
+export const Progress: Story = {
+  name: 'Progress, visible'
+}
 
 export const LinearType1: Story = {
   name: 'Linear, indeterminate=type1',
