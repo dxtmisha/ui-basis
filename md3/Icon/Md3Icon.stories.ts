06369a0fb41a221705b4997b85679070a354d936
dxtmisha <9943154+dxtmisha@users.noreply.github.com>
2023-08-06 21:43:03 +0700

    Md3Icon: fix

--- md3/Icon/Md3Icon.stories.ts
+++ md3/Icon/Md3Icon.stories.ts
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/vue3'
 import Md3Icon from './Md3Icon.vue'
 import { stories } from '../stories'
 
-import { active, disabled, hide, icon, turn } from '../stories/argTypes'
+import { active, disabled, hide, icon, onLoad, turn } from '../stories/argTypes'
 
 // More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
 const meta = {
@@ -90,17 +90,21 @@ const meta = {
         }
       }
       // :arg-types
-    }
+    },
+    onLoad
   },
   args: {
-    icon: 'home',
-    ...{
-      // [!] System label, cannot be deleted
-      // [!] Системная метка, нельзя удалять
-      // :default
-      // :default
-    }
-  }
+    icon: 'home'
+  },
+  decorators: [
+    () => ({
+      template: `
+        <div style="display: inline-flex; align-items: center; justify-content: center; gap: 8px;">
+          <story/>
+        </div>
+      `
+    })
+  ]
 } satisfies Meta<typeof Md3Icon>
 
 type Story = StoryObj<typeof meta>;
@@ -108,6 +112,49 @@ type Story = StoryObj<typeof meta>;
 export default meta
 
 export const Icon: Story = {
-  // name: 'Icon',
-  args: {}
+  name: 'Icon, value=home'
+}
+
+export const IconActive: Story = {
+  name: 'Icon, active=true',
+  args: {
+    iconActive: 'face'
+  },
+  render: (args) => ({
+    components: { Md3Icon },
+    setup () {
+      return { args }
+    },
+    template: `
+      <md3-icon v-bind="args" :active="false"/>
+      <md3-icon v-bind="args" :active="true"/>
+    `
+  })
+}
+
+export const IconDynamic: Story = {
+  name: 'Icon, dynamic=true',
+  args: {
+    dynamic: true
+  }
+}
+
+export const ImageRounded: Story = {
+  name: 'Image, size=<string>',
+  args: {
+    icon: icon.options[icon.options.length - 1]
+  },
+  render: (args) => ({
+    components: { Md3Icon },
+    setup () {
+      return { args }
+    },
+    template: `
+      <md3-icon v-bind="args" size="xs"/>
+      <md3-icon v-bind="args" size="sm"/>
+      <md3-icon v-bind="args" size="md"/>
+      <md3-icon v-bind="args" size="lg"/>
+      <md3-icon v-bind="args" size="xl"/>
+    `
+  })
 }
